b247da5cf68a5af5b2825979f313ca22bef67d34
Abdlrhman <abdlrhmanv@icloud.com>
2025-08-03 20:18:46 +0300

    2. Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,16 @@
 import React from 'react'
+import About from '@/pages/about'
+import Home from '@/pages/home'
+import Link from 'next/link'
+
+
+const Header = () => {
+  return (
+    <div className='flex gap-4'>
+        <Link href="/about">About</Link>
+        <Link href="/home">Home</Link>
+    </div>
+  )
+}
+
+export default Header;  
